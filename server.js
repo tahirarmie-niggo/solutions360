56891cde09ae725d0fc93d42e40f06691f1ef207
tahirarmie-niggo <tahirarmie@gmail.com>
2026-05-03 14:33:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Security
 app.use(helmet({ contentSecurityPolicy: false }));
